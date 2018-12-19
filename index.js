3e93a4419f09eca7de9bad3c673d9c4b7ce30e67
김태욱 <kammat136@gmail.com>
2018-12-19 17:37:35 +0900

    fix typo

--- index.js
+++ index.js
@@ -184,8 +184,8 @@ app.get("/deletecomment", function(req, res) {
     else console.log(comment);
 
     //var newId = new mongoose.mongo.ObjectId(req.query.idofcomment);
-    //comment.comments.pull({ contents: req.query.commentcontents});
-    comment.pull({“comments”: { “contents”: req.query.commentcontents } });
+    comment.comments.pull({ contents: req.query.commentcontents});
+    //comment.pull({“comments”: { “contents”: req.query.commentcontents } });
 
     comment.save(function(err) {
       if (err) res.status(500);
