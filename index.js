5e7309d68abf593b4df9559d3ad09e628bb5bb0a
김태욱 <kammat136@gmail.com>
2018-12-20 21:28:37 +0900

    add gfs variable

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const GridFsStorage = require('multer-gridfs-storage');
 
 var bodyParser = require('body-parser')
 
+let gfs;
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
