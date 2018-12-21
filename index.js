6f8acaa90d2528853c9fc4494d8819caf8880291
김태욱 <kammat136@gmail.com>
2018-12-21 16:58:21 +0900

    revert

--- index.js
+++ index.js
@@ -179,8 +179,6 @@ app.get("/getcomments", function(req, res) {
     if (err) return res.status(500);
     else console.log(comment);
 
-    comment.comments.update($sort: { comment_date: 1 });
-
     if(comment != null) res.send(comment);
   });
 });
