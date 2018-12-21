b745199738b5eaef509366219b8de4aa31edb791
김태욱 <kammat136@gmail.com>
2018-12-21 17:48:43 +0900

    please help me

--- index.js
+++ index.js
@@ -173,7 +173,10 @@ app.get("/deletepick", function(req, res) {
 
 app.get("/getcomments", function(req, res) {
   //Userauthid, longitude, longitude를 받아서 댓글 추가.//글 고유번호
-  db.Data.findOne({ _id: req.query.userauthid}, {sort: {comment_date: 1}}, function(err, comment) {
+  db.Comment.sort([['comment_date', -1]]).exec(function(err, docs) {
+    
+  });
+  db.Data.findOne({ _id: req.query.userauthid}, function(err, comment) {
     if (err) return res.status(500);
     else console.log(comment);
 
